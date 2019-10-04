6a3945f2f873bc6d7680407d813fb32f28a39656
Pedro Bini <dev.pedrobini@gmail.com>
2019-10-04 17:02:29 -0300

    Add Bootstrap values

---
+++ src/breakpoints.js
@@ -0,0 +1,25 @@
+/**
+ * Defines breakpoint values according to bootstrap 4.
+ */
+export const breakpoints = {
+  xs: {
+    height: undefined,
+    widht: 575,
+  },
+  sm: {
+    height: undefined,
+    width: 767,
+  },
+  md: {
+    height: undefined,
+    width: 991,
+  },
+  lg: {
+    height: undefined,
+    width: 1199,
+  },
+  xl: {
+    height: undefined,
+    width: 1920,
+  },
+};
---
+++ src/layout.js
@@ -0,0 +1,10 @@
+/**
+ * Defines layout breakpoints for bootstrap's layout names: xs, sm, md, lg, xl.
+ */
+export const layout = {
+  xs: 1,
+  sm: 2,
+  md: 3,
+  lg: 4,
+  xl: 5,
+};
