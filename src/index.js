8d66790f9e3ac44c0fbc1c4c4e6a19527fd78ca9
Pedro Bini <dev.pedrobini@gmail.com>
2019-10-04 17:02:40 -0300

    Add useWindowSize hook

--- src/index.js
+++ src/index.js
@@ -1,21 +1,72 @@
-import * as React from 'react'
+import { useState, useEffect } from "react";
+import { breakpoints } from "./breakpoints";
+import { layout } from "./layout";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+const getLayout = (width) => {
+  if (width >= breakpoints.xl.width) {
+    return layout.xl;
+  }
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
+  if (width >= breakpoints.lg.width) {
+    return layout.lg;
+  }
+
+  if (width >= breakpoints.md.width) {
+    return layout.md;
+  }
+
+  if (width >= breakpoints.sm.width) {
+    return layout.sm;
+  }
+
+  return layout.xs;
+};
+
+const getSize = (isClient) => {
+  const width = isClient
+    ? "innerWidth" in window
+      ? window.innerWidth
+      : document.documentElement.offsetWidth
+    : undefined;
+
+  const height = isClient
+    ? "innerHeight" in window
+      ? window.innerHeight
+      : document.documentElement.offsetHeight
+    : undefined;
+
+  return {
+    screenLayout: getLayout(width),
+    height,
+    width,
+  };
+};
+
+/**
+ * Hook that monitors window size, and updates the object
+ * at the end of each window resize. It also returns the
+ * actual screen layout - one of xl, lg, md, sm, xs.
+ */
+export default function useWindowSize() {
+  const isClient = typeof window === "object";
+
+  const [windowSize, setWindowSize] = useState(getSize(isClient));
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
     }
-  }, [])
 
-  return counter
+    let resizeId;
+
+    const handleResize = () => {
+      clearTimeout(resizeId);
+      resizeId = setTimeout(() => setWindowSize(getSize(isClient)), 200);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
 }
