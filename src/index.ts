82ca5823a0b90a6eb1bf8c04e2e28b5f695457e2
Pedro Bini <dev.pedrobini@gmail.com>
2020-06-16 18:21:49 -0300

    Remove default export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import useWindowSize from './hooks/useWindowSize';
 import LayoutEnum from './constants/layoutEnum';
 
-export default useWindowSize;
-
 export {
   LayoutEnum as layout,
   useWindowSize,
